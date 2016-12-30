abf1e6a1426d862714ac1fb6fcfc5e99730d59cd
herolee <herolee@gmail.com>
2016-12-30 16:03:17 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #安装云监工环境脚本
 #sanzuwu@gamil.com
-#脚本适用ubuntu14、debian8
-
+#脚本适用CentOS
 #更新源
 
-sudo apt-get update 
+yum update 
 
 #安装python3.4
 
-sudo apt-get install -y python3.4 python3.4-dev 
+sudo yum install -y python3.4
 
 #安装pip，确保本脚本和get-pip.py 文件在一个文件夹
 BASE_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
@@ -19,7 +18,7 @@ sudo python3.4 ${BASE_DIR}/get-pip.py
 sudo pip3.4 install redis && sudo pip3.4 install requests && sudo pip3.4 install flask && sudo pip3.4 install flask-mail
 
 #安装redis-server
-sudo apt-get install -y redis-server 
+sudo yum install -y redis
 
 
 #运行云监工
